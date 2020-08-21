6de182e77e64967c3a9b05cccbce0360422e21c6
Aristide Poudjom <poudjom@gmail.com>
2020-08-21 18:15:21 +0100

    Avant cours sur l'administation django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length = 100)
+    auteur = models.CharField(max_length = 100)
+    contenu = models.TextField(null = True)
+    slug = models.SlugField(max_length=100, default="")
+    date = models.DateField(default = timezone.now, verbose_name="Date publication")
+    nombrevues = models.IntegerField(default = 0, verbose_name = "Nombre de lecture")
+    categorie = models.ForeignKey("categorie", on_delete=models.PROTECT)
+    
+    class Meta:
+        verbose_name = "Article"
+        ordering = ['date']    
+        
+        
+    def __str__(self):
+        return self.titre
+
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length = 30)
+     
+    class Meta:
+        verbose_name  = "Catégorie"
+         
+    def __str__(self):
+        return self.nom
+         
--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('acceuil', views.home),
-    path('list_article', views.list_article, name = 'listearticle'),
+    path('acceuil', views.home, name = "acceuil"),
+    path('lire/<int:id>-<slug:slug>$', views.lire_article, name = 'lire'),
     path('redirection', views.view_redirection, name = 'redirection'),
     path('date', views.date, name = 'date'),
     path('addition/<int:nombre1>/<int:nombre2>', views.addition, name = 'addition'),
